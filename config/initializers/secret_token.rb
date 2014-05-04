38a4994152400581e0732051aa723b36490f36b6
Rafael Medina <rmedinap@gmail.com>
2014-05-03 19:07:21 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CardStore::Application.config.secret_key_base = '13f79941b8a4b0f2b444bf724ae8f53e44359846c19fea513c49e721b5deb8383430e891a26686d8ade26bf51b2e9f65be48d3332892d4a84dd4b6a9f13edbf8'
