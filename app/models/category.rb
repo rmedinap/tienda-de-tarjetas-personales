766e71918874a71147a56ce52bcb7101841ca3c5
Rafael Medina <rmedinap@gmail.com>
2014-05-04 09:12:50 -0500

    Category - Product: Relationship.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
+# coding: utf-8
 class Category < ActiveRecord::Base
+  has_many :products
+
+  def to_s
+    name
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 # coding: utf-8
 class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
+
+  belongs_to :category
 end
